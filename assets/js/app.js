e4eadbf68dbdec717969fb4ab2845a6989e5c371
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-05 09:35:09 -0500

    removes javascript files and new tab on form submit action

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-
-//     $(".form-control").val("");
-    
-//     $("#submit").on("click", function() {
-//         $(".modal").modal("show");
-//     });
-
-// });
