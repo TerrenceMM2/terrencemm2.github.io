8774bc4d57daaf90f2b0e0aa354de79e8af74d8d
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-05 09:18:54 -0500

    edits modal message on form submission

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     
     $("#submit").on("click", function() {
-        console.log("click");
-        // event.preventDefault();
         $(".modal").modal("show");
         $(".form-control").val("");
     });
