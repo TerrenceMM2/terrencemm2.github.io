27f00109916a7fa8191eeec33e5ec38b5a1c7ec2
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-24 17:04:00 -0500

    adds captions to portfolio images

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,32 +1,3 @@
 $(document).ready(function () {
 
-    $(".nav-link").on("click", function () {
-
-        var click = $(this).attr("href");
-        var about = "#about";
-        var portfolio = "#portfolio";
-        var contact = "#contact";
-
-        switch (click) {
-            case about:
-                $(portfolio).fadeOut().hide();
-                $(contact).fadeOut().hide();
-                $(about).fadeIn().show();
-                break;
-            case portfolio:
-                $(about).fadeOut().hide();
-                $(contact).fadeOut().hide();
-                $(portfolio).fadeIn().show();
-                break;
-            case contact:
-                $(about).fadeOut().hide();
-                $(portfolio).fadeOut().hide();
-                $(contact).fadeIn().show();
-                break;
-            default:
-                $(about).fadeIn().show();
-                $(portfolio).fadeOut().hide();
-                $(contact).fadeOut().hide();
-        };
-    });
 });
