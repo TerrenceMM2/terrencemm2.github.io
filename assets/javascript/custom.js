0ba7a441eeca3d794c9084aa527724f728e8e81f
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-09 09:51:54 -0500

    adds local fonts and fixes footer on portfolio page in mobile view

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -4,30 +4,35 @@ $(document).ready(function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("footer").addClass("position-relative");
     });
 
     $("#portfolioLink").on("click", function() {
         $("#portfolio").show();
         $("#about").hide();
         $("#contact").hide();
+        $("footer").removeClass("position-relative");
     });
 
     $("#contactLink").on("click", function() {
         $("#contact").show();
         $("#portfolio").hide();
         $("#about").hide();
+        $("footer").addClass("position-relative");
     });
 
     $("#logo").on("click", function() {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("footer").addClass("position-relative");
     });
 
     $("#logo-name").on("click", function() {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("footer").addClass("position-relative");
     });
 
 
