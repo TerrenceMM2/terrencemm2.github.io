5ef995498916cd8ee0627927e28ac51eb4e8c8d3
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-31 08:53:54 -0500

    updates portfolio structure, removes carousel, and removes footer JS logic

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -4,58 +4,30 @@ $(document).ready(function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
-        if (screen.width < 414) {
-            $("footer").removeClass("position-absolute");
-            $("footer").addClass("position-relative");
-        }
     });
 
     $("#portfolioLink").on("click", function () {
         $("#portfolio").show();
         $("#about").hide();
         $("#contact").hide();
-        if (screen.width < 414 && screen.height < 630) {
-            $("footer").removeClass("position-relative");
-            $("footer").addClass("position-absolute");
-        }
-        if (screen.width < 414 && screen.height > 630) {
-            $("footer").removeClass("position-relative");
-            $("footer").addClass("position-absolute");
-        }
     });
 
     $("#contactLink").on("click", function () {
         $("#contact").show();
         $("#portfolio").hide();
         $("#about").hide();
-        if (screen.width < 414 && screen.height < 630) {
-            $("footer").removeClass("position-absolute");
-            $("footer").addClass("position-relative");
-        }
-        if (screen.width < 414 && screen.height > 630) {
-            $("footer").removeClass("position-relative");
-            $("footer").addClass("position-absolute");
-        }
     });
 
     $("#logo").on("click", function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
-        if (screen.width < 414) {
-            $("footer").removeClass("position-absolute");
-            $("footer").addClass("position-relative");
-        }
     });
 
     $("#logo-name").on("click", function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
-        if (screen.width < 414) {
-            $("footer").removeClass("position-absolute");
-            $("footer").addClass("position-relative");
-        }
     });
 
 
