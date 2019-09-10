022e0c804c91a35af8bb4a7090ad3695952c6b45
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-10 11:07:59 -0500

    adds skills pages

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -4,30 +4,42 @@ $(document).ready(function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("#skills").hide();
     });
 
     $("#portfolioLink").on("click", function () {
         $("#portfolio").show();
         $("#about").hide();
         $("#contact").hide();
+        $("#skills").hide();
+    });
+
+    $("#skillsLink").on("click", function () {
+        $("#skills").show();
+        $("#contact").hide();
+        $("#portfolio").hide();
+        $("#about").hide();
     });
 
     $("#contactLink").on("click", function () {
         $("#contact").show();
         $("#portfolio").hide();
         $("#about").hide();
+        $("#skills").hide();
     });
 
     $("#logo").on("click", function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("#skills").hide();
     });
 
     $("#logo-name").on("click", function () {
         $("#about").show();
         $("#portfolio").hide();
         $("#contact").hide();
+        $("#skills").hide();
     });
 
 
